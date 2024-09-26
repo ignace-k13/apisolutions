447250b3b6b404786ab810ae60492dd300df3a2d
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2024-09-26 08:05:39 +0200

    API basics

---
+++ Exercise12/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -0,0 +1,21 @@
+package be.abis.exercise.controller;
+
+import be.abis.exercise.model.Person;
+import be.abis.exercise.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+	@Autowired
+	PersonService ps;
+
+	@GetMapping("persons")
+	public Person findPersonByMailAndPwd(@RequestParam("mail") String emailAddress, @RequestParam("pwd") String passWord) {
+		return ps.findPerson(emailAddress, passWord);
+	}
+
+}
